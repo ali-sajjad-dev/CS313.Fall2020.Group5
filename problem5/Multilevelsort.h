c9e94e1f5a79fab3af180ce96340d4acdde096bd
asajjad38 <49500921+asajjad38@users.noreply.github.com>
2020-10-07 10:13:25 -0400

    Update Multilevelsort.h

--- problem5/Multilevelsort.h
+++ problem5/Multilevelsort.h
@@ -1,9 +1,5 @@
 #pragma once
 
-void mergesort(int array[], int start, int end);
-
-void merge(int array[], int start, int mid, int end);
-
 void multiLevelSort(int array[], int start, int end);
 
 void mergeHalves(int array[], int start, int mid, int end);
