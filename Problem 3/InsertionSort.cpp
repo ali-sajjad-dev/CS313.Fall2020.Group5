cc2399286db0ef5de98959c27a338146abc43834
Joshuarampersaud <72413193+Joshuarampersaud@users.noreply.github.com>
2020-10-07 10:03:40 -0400

    Add files via upload

---
+++ Problem 3/InsertionSort.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <bits/stdc++.h> 
+
+using namespace std; 
+  
+int main()  
+{  
+    int arr[] = { 12, 11, 13, 5, 6 };  
+    int n = sizeof(arr) / sizeof(arr[0]);  
+  
+    insertionSort(arr, n);  
+    printArray(arr, n);  
+  
+    return 0;  
+}
+
+void insertionSort(int arr[], int n)  
+{  
+    int i, key, j;  
+    for (i = 1; i < n; i++) 
+    {  
+        key = arr[i];  
+        j = i - 1;  
+ 
+        while (j >= 0 && arr[j] > key) 
+        {  
+            arr[j + 1] = arr[j];  
+            j = j - 1;  
+        }  
+        arr[j + 1] = key;  
+    }  
+}  
+     
+  
